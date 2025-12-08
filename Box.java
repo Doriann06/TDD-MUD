479c0358d8f924744d7f2c41412470ee869b7413
Doriann Bachelet <doriann.bak@gmail.com>
2025-12-08 19:04:12 +0100

    test classe remove

--- Box.java
+++ Box.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertFalse;
+
 import java.util.ArrayList;
 
 public class Box{
@@ -12,4 +14,11 @@ public void add(Thing truc){
 public boolean contains(Thing truc){
     return this.contents.contains(truc);
 }
+public void remove(Thing truc){
+    contents.remove(truc);
+    if(this.contents.contains(truc)==false){
+        new Exception("truc n'est pas dans liste");
+
+    }
+    }
 }
--- TestBoxes.java
+++ TestBoxes.java
@@ -19,7 +19,23 @@ public class TestBoxes{
         assertTrue( b.contains(truc1));
         assertTrue( b.contains(truc2));
         assertFalse(b.contains(truc3));
-
+        
+    }
+    @Test
+    public void testBoxRemove(){
+        
+        Box b =new Box();
+        Thing truc1 =new Thing("truc1");
+        Thing truc2 =new Thing("truc2");
+        Thing truc3 =new Thing("truc3");
+       
+        b.add(truc1);
+        b.add(truc2);
+        b.remove(truc2);
+        b.remove(truc3);
+        assertTrue( b.contains(truc1));
+        assertFalse(b.contains(truc2));
+        assertFalse(b.contains(truc3));
         
     }
 }
